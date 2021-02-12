125b87357d74e739cff87a8396fa0d812eb58a99
diegodrts <63802389+diegodrts@users.noreply.github.com>
2021-02-12 12:48:46 -0300

    Review framesCoef.m
    I've just reviewed my old code, it may need some comments to explain argsin* data structure.

--- Open DaS MMUS (DAQ)/framesCoef.m
+++ Open DaS MMUS (DAQ)/framesCoef.m
@@ -3,31 +3,35 @@ function [coef_map, header] = framesCoef(rfdata, header, roi)
 %ultrassonicos. Baseado em correlacao cruzada unidimensional e ajuste de
 %pico maximo bilinear ou polinomial de ordem 2.
 %
-% [map, map_0] = framesElastoSL(RFMAPS,HEADER,ROI,TYPE)
+% [map, map_0] = framesElastoSL(RFMAPS, HEADER, ROI, TYPE)
 %
 % See also FRAMESLOAD, FRAMESARRANGE
 
 % Author(s): D.R.T. Sampaio
 % $Revision: 1.0 $  $Date: 27-Fev-2014 17:20:04 $
+% Githubbed: 11-fev-2021
 %
 % References:
 %
 %
 
-agonia = waitbar(0,'RAM: Coeficiente de correlação - VMA Framework');
-
+agonia = waitbar(0,'RAM: Coeficiente de correlaÃ§Ã£o - MMUS');
 
 % region of interest
-roi_h = roi.yi:roi.yf;
+% it's possible to hardcode the boundaries of the ROI
+% if it's a square-ROI, differences between the axial and lateral resolutions
+% must be compensated.
+roi_h = roi.yi:roi.yf; 
 roi_w = roi.xi:roi.xf;
 
 % parameters
-fs = header.us.sf;
-depth =  header.mg.depth;
+% it's possible to hardcode the US parameters
+fs         = header.us.sf;
+depth      =  header.mg.depth;
 soundspeed = header.mg.soundspeed;
 
 height = size(roi_h,2);
-width = size(roi_w,2);
+width  = size(roi_w,2);
 
 % frame interval
 df = header.pp.dt;
@@ -42,19 +46,21 @@ if (ff <= f0),
     ff = size(rfdata,2);
 end
 
-win_size = header.pp.winsize; % window and shift
+win_size    = header.pp.winsize; % window and shift
 win_overlap = header.pp.overlap;
-win_shift = win_size * (1 - win_overlap);
+win_shift   = win_size * (1 - win_overlap);
 
 win_size_px = fix((win_size*1e-3) * 2 * fs/soundspeed); % window and shift px
-shift_px = fix((win_shift*1e-3) * 2 * fs/soundspeed);
+shift_px    = fix((win_shift*1e-3) * 2 * fs/soundspeed);
+
+% inicializaton for frame
+% TODO: I don't remember what is this "bound"
+% may improve it (shout coding)
+bound = 7; 
 
 %% Coeffient 
 %
-
-% inicializaton for frame
-bound = 7;
-m = 1;
+m     = 1;
 f = f0;
 disp('ram')
 tic
@@ -68,7 +74,7 @@ while f + df <= ff,
     n = 1;
     
     % boundary
-    A = zeros(length(roi_h),bound-1);
+    A = zeros(length(roi_h), bound-1);
     
     % frames ensemble
     rf_0 = [A, rfdata{f0}(roi_h,roi_w), A];
@@ -96,6 +102,8 @@ while f + df <= ff,
     
     f = f + df;
     
+    % TODO: I don't remember what is this part
+    % may improve it (shout coding)
     if ((m == 1)&&(f0 == 1)),
         Mro = mean(mean(ro(:,:)));
     else
@@ -103,7 +111,7 @@ while f + df <= ff,
     end
     
     %coef{m} = ro(:,:) - Mro;
-    coef_map{m} = (ro(:,:) - 1) * -1;
+    coef_map{m} = (ro(:, :) - 1) * -1;
     
     m = m + 1;
 end
